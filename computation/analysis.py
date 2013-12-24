9d9461336d01d6753421c5d978f66cdb05c44f02
NerdHerd91 <sventrel@outlook.com>
2013-12-23 19:12:57 -0800

    Messing around with matplotlib

--- computation/analysis.py
+++ computation/analysis.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 class Analysis:
 	
 	def __init__(self, name):
@@ -5,3 +8,7 @@ class Analysis:
 
 	def run(self):
 		print "We are Running: " + self.name
+		x = np.array(range(20))
+		y = 3 + 0.5 * x + np.random.randn(20)
+		plt.plot(x,y, 'bo')
+		plt.show()
